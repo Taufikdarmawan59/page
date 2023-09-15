fa0c35defb55b5a99bfc7e48097edae9d509ab86
bangtop <59765368+Taufikdarmawan59@users.noreply.github.com>
2023-09-15 13:33:57 +0700

    pages

---
+++ js/bot.js
@@ -0,0 +1,43 @@
+(function ($) {
+    "use strict";
+        // Spinner
+        var spinner = function () {
+            setTimeout(function () {
+                if ($('#spinner').length > 0) {
+                    $('#spinner').removeClass('show');
+                }
+            }, 1);
+        };
+        spinner();
+      
+        // Initiate the wowjs
+    new WOW().init();
+
+      window.addEventListener('DOMContentLoaded', event => {
+
+            // Activate Bootstrap scrollspy on the main nav element
+            const mainNav = document.body.querySelector('#mainNav');
+            if (mainNav) {
+                new bootstrap.ScrollSpy(document.body, {
+                    target: '#mainNav',
+                    offset: 74,
+                });
+            };
+
+            // Collapse responsive navbar when toggler is visible
+            const navbarToggler = document.body.querySelector('.navbar-toggler');
+            const responsiveNavItems = [].slice.call(
+                document.querySelectorAll('#navbarResponsive .nav-link')
+            );
+            responsiveNavItems.map(function (responsiveNavItem) {
+                responsiveNavItem.addEventListener('click', () => {
+                    if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                        navbarToggler.click();
+                    }
+                });
+            });
+
+        });
+
+      // layanan yang diberikan
+})(jQuery);
